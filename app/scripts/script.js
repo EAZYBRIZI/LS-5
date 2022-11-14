5f3afad6e68572262d548d1a520846181771f9c6
EAZYBRIZI <gusarov.andreq@yandex.ru>
2022-11-15 02:14:57 +0300

    first landing

---
+++ app/scripts/script.js
@@ -0,0 +1,11 @@
+email.onclick = () => {
+    navigator.clipboard.writeText('gusarov.andreq@yandex.ru');
+}
+
+phoneNumber.onclick = () => {
+    navigator.clipboard.writeText('8-953-426-87-44');
+}
+
+telegram.onclick = () => {
+    navigator.clipboard.writeText('@ezbizi');
+}
